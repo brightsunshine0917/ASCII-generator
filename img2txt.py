388e84b7a70c774f1b3ed487842ed3f74b1986e0
brightsunshine0917 <brightsunshine0917@email.email>
2024-11-22 12:32:34 +0800

    refactor(img2txt): improve code readability

--- img2txt.py
+++ img2txt.py
@@ -39,16 +39,19 @@ def main(opt):
         CHAR_LIST = "@%#*+=-:. "
     else:
         CHAR_LIST = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+
     num_chars = len(CHAR_LIST)
     num_cols = opt.num_cols
+
     image = cv2.imread(opt.input)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     height, width = image.shape
     cell_width = width / opt.num_cols
     cell_height = 2 * cell_width
     num_rows = int(height / cell_height)
+
     if num_cols > width or num_rows > height:
-        print("Too many columns or rows. Use default setting")
+        print("Too many columns or rows. Using default setting")
         cell_width = 6
         cell_height = 12
         num_cols = int(width / cell_width)
@@ -57,25 +60,15 @@ def main(opt):
     output_path = Path(opt.output)
     output_path.parent.mkdir(exist_ok=True, parents=True)
     with open(output_path, "w") as f:
-        for i in range(num_rows):
-            for j in range(num_cols):
-                f.write(
-                    CHAR_LIST[
-                        min(
-                            int(
-                                np.mean(
-                                    image[
-                                        int(i * cell_height) : min(int((i + 1) * cell_height), height),
-                                        int(j * cell_width) : min(int((j + 1) * cell_width), width),
-                                    ]
-                                )
-                                * num_chars
-                                / 255
-                            ),
-                            num_chars - 1,
-                        )
-                    ]
-                )
+        for rowno in range(num_rows):
+            for colno in range(num_cols):
+                block = image[
+                    int(rowno * cell_height) : min(int((rowno + 1) * cell_height), height),
+                    int(colno * cell_width) : min(int((colno + 1) * cell_width), width),
+                ]
+                avg_brightness = np.mean(block) * num_chars / 255
+                char = CHAR_LIST[min(int(avg_brightness), num_chars - 1)]
+                f.write(char)
             f.write("\n")
     print(f"Output has been saved to {output_path}")
 
